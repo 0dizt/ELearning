dbabbd65e3aba0717113d50e1600ed7cbcfc71f9
joshnwosu <joshuanwosu078@gmail.com>
2023-01-08 20:19:38 +0100

    update

--- src/screens/MasonryLayout.jsx
+++ src/screens/MasonryLayout.jsx
@@ -1,61 +1,50 @@
 import React from "react";
-import { FlatList, View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import MasonryList from "@react-native-seoul/masonry-list";
 
 const data = [
-  { key: "a", h: 50 },
-  { key: "b", h: 100 },
-  { key: "c", h: 50 },
-  { key: "d", h: 100 },
-  { key: "e", h: 50 },
-  { key: "f", h: 100 },
-  { key: "g", h: 50 },
-  { key: "h", h: 100 },
-  { key: "i", h: 50 },
-  { key: "j", h: 100 },
-  { key: "k", h: 50 },
-  { key: "l", h: 100 },
-  { key: "m", h: 50 },
-  { key: "n", h: 100 },
-  { key: "o", h: 50 },
-  { key: "p", h: 100 },
-  { key: "q", h: 50 },
-  { key: "r", h: 100 },
-  { key: "s", h: 50 },
-  { key: "t", h: 100 },
+  { key: "a", height: 300 },
+  { key: "b", height: 500 },
+  { key: "c", height: 300 },
+  { key: "d", height: 500 },
+  { key: "e", height: 300 },
+  { key: "f", height: 500 },
   // ...
 ];
 
-const numColumns = 3;
-
-const Item = ({ item }) => (
-  <View style={[styles.item, { height: item.h }]}>
-    <Text>{item.key}</Text>
+const Item = ({ item, index }) => (
+  <View style={[styles.item, { height: item.height }]}>
+    <Text style={styles.text}>
+      {item.key}
+      {index}
+    </Text>
   </View>
 );
 
 const MasonryLayout = () => (
-  <FlatList
-    data={data}
-    renderItem={Item}
-    numColumns={numColumns}
-    itemLayout={(data, index) => ({
-      length: ITEM_WIDTH,
-      offset: ITEM_WIDTH * index,
-      index,
-    })}
-  />
+  <>
+    <MasonryList
+      data={data}
+      keyExtractor={(item) => item.key}
+      numColumns={2}
+      showsVerticalScrollIndicator={false}
+      renderItem={Item}
+    />
+  </>
 );
-
-const ITEM_WIDTH = 100;
 
 const styles = StyleSheet.create({
   item: {
-    width: ITEM_WIDTH,
-    // height: ITEM_WIDTH,
-    backgroundColor: "#333",
+    backgroundColor: "#F570C9",
     margin: 5,
     alignItems: "center",
     justifyContent: "center",
+    borderRadius: 10,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "600",
+    color: "#ffffff",
   },
 });
 
